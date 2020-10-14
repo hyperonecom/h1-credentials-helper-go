cb49db1522d8b9b23a06e31cda0e7818e164b6c3
kkm <kubasurdej@gmail.com>
2020-10-14 21:05:27 +0200

    Create test for creating passport provider

---
+++ h1_test.go
@@ -0,0 +1,14 @@
+package h1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreatingPassportProvider(t *testing.T) {
+	_, err := GetPassportCredentialsHelper("providers/passport/fixtures/validPassport.json")
+	require.NoError(t, err)
+	_, err = GetPassportCredentialsHelper("providers/passport/fixtures/invalidPassport.json")
+	require.Error(t, err)
+}
